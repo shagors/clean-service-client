17096377f0e5e224a37b8c7c0cf9b8d7cd01399e
shagors <shajjadshagor@gmail.com>
2022-05-19 15:56:18 +0600

    animation add and home page design complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { PublicRoute } from "./routes/PublicRoutes";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect( () => {
+    AOS.init();
+  } , [])
+
   return (
       <Navbar>
         <Routes>
