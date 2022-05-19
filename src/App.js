3b84e09c7cb81a090270e3e28007476852ffc880
shagors <shajjadshagor@gmail.com>
2022-05-19 12:41:58 +0600

    navbar add and link it and drop down options add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/services" element={<Services></Services>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({children}) => {
     return (
         <div className="drawer drawer-end">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
             <div className="drawer-content flex flex-col">
-                <div className="w-full navbar bg-base-300">
+                <div className="w-full navbar bg-base-100 px-20">
                 <div className="flex-1 px-2 mx-2 text-2xl">Clean Co.</div>
                 <div className="flex-none lg:hidden">
                     <label for="my-drawer-3" className="btn btn-square btn-ghost">
@@ -13,18 +14,42 @@ const Navbar = () => {
                     </label>
                 </div>
                 <div className="flex-none hidden lg:block">
-                    <ul className="menu menu-horizontal">
-                    <li><a>Navbar Item 1</a></li>
-                    <li><a>Navbar Item 2</a></li>
+                    <ul className="menu menu-horizontal gap-x-2">
+                        <li>
+                            <NavLink to='/home' className='rounded-lg'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about' className='rounded-lg'>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/services' className='rounded-lg'>Services</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contact' className='rounded-lg'>Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/login' className='rounded-lg'>Login</NavLink>
+                        </li>
+                        <li class = "dropdown dropdown-hover dropdown-end" >
+                            <label tabindex="0" class="btn btn-primary btn-outline rounded-lg m-1">Book Now</label>
+                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Item 1</a></li>
+                                <li><a>Item 2</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
                 </div>
+                {children}
             </div>
             <div className="drawer-side">
                 <label for="my-drawer-3" className="drawer-overlay"></label> 
                 <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Services</a></li>
+                <li><a>Contact</a></li>
+                <li><a>Login</a></li>
                 </ul>
             </div>
         </div>
