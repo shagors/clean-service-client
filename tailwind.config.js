e414d96840a7ca28e3dae4596bf33ab85ff93503
shagors <shajjadshagor@gmail.com>
2022-05-19 11:46:00 +0600

    navbar add and responsive for mobile

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [{
+      mytheme: {
+        "primary": "#176F6B",
+        "secondary": "#ffc000",
+        "accent": "#ffedd5",
+        "neutral": "#f3f4f6",
+        "base-100": "#FFFFFF",
+        "info": "#98a8dd",
+        "success": "#1bbb70",
+        "warning": "#df7e07",
+        "error": "#fa5c5c",
+      },
+    },],
+  },
+  plugins: [require("daisyui")],
+}
