544b3f8b765cfda4d0c2914da61130e6b1555151
shagors <shajjadshagor@gmail.com>
2022-05-21 16:35:46 +0600

    connect with db and data fetch with axios and data check

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import fetcher from '../api';
 
 const Services = () => {
+    const [services, setServices] = useState({});
+    console.log(services);
+
+    useEffect( () => {
+        // axios.get('/service').then(res => setServices(res));
+
+        (async () => {
+            const res = await fetcher.get("/service");
+            setServices(res.data);
+        })();
+    }, []);
+
     return (
-        <div>
+        <div className='mt-16'>
             <h1>Services</h1>
             <h1>Services</h1>
             <h1>Services</h1>
