684f572ae95ff1d3c2b133e0403c91d95d0668a0
shagors <shajjadshagor@gmail.com>
2022-05-21 11:54:04 +0600

    design dashboard and protected route set and nested route set

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { PublicRoute } from "./routes/PublicRoutes";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
-import Services from "./pages/Services";
 import PrivateRoute from "./authentication/PrivateRoute";
 import { privateRoutes } from "./routes/privateRoutes";
+import AdminRoute from "./authentication/AdminRoute";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AddService from "./pages/Dashboard/AddService";
+import AddAdmin from "./pages/Dashboard/AddAdmin";
 
 function App() {
 
@@ -24,6 +27,12 @@ function App() {
             {
               privateRoutes.map(({ path, Component }, index) => (<Route key={index} path={path} element={<Component />}></Route>))
             }
+          </Route>
+          <Route element={<AdminRoute></AdminRoute>}>
+            <Route path="/dashboard" element={<Dashboard></Dashboard>}>
+                <Route path = 'add-service' element={<AddService></AddService>}></Route >
+                <Route path = 'add-admin'element={<AddAdmin></AddAdmin>}></Route>
+            </Route>
           </Route>
         </Routes>
       </Navbar>
---
+++ src/pages/Dashboard/AddService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddService = () => {
+    return (
+        <div>
+            <h1>Service Add List</h1>
+        </div>
+    );
+};
+
+export default AddService;
